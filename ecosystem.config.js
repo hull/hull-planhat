1a2089c83082764e0315044f9ff1f419e99827a3
Sven Maschek <sven@gritbase.co>
2019-09-03 10:43:06 +0200

    Add pm2 for Heroku deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps : [{
+      name: 'Hull Planhat',
+      script: 'build/index.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '450M',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }],
+  };
+  
