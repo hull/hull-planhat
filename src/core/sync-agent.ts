437b60d43e9b26f938e3931f5933723a3b686fac
Sven Maschek <smaschek@outlook.com>
2020-04-29 23:17:55 +0200

    Parse switch for current job

--- src/core/sync-agent.ts
+++ src/core/sync-agent.ts
@@ -902,9 +902,6 @@ class SyncAgent {
 
     const jobInfo = await redisClnt.get<string>(cacheKey);
 
-    // eslint-disable-next-line no-console
-    console.log(">>> Last Job Info", jobInfo, typeof jobInfo);
-
     if (_.isNil(jobInfo) || jobInfo === undefined) {
       return undefined;
     }
@@ -927,11 +924,15 @@ class SyncAgent {
 
     const jobInfo = await redisClnt.get<string>(cacheKey);
 
-    if (_.isNil(jobInfo)) {
+    if (_.isNil(jobInfo) || jobInfo === undefined) {
       return undefined;
     }
 
-    return JSON.parse(jobInfo) as IncomingFetchJob;
+    if (_.isString(jobInfo)) {
+      return JSON.parse(jobInfo) as IncomingFetchJob;
+    }
+
+    return jobInfo as IncomingFetchJob;
   }
 
   private async upsertLastJob(
